7885e3a75fc88685b1ea8e18e09824cdc7145283
Dhairya <risingshadows.bugs@gmail.com>
2025-04-12 11:30:16 +0530

    Updated

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ app.use(express.static(path.join(__dirname,"public")))
 
 let score = function(att, wro){
     let total= 300;
-    let positive = (75-att)*4;
+    let positive = (att-wro)*4;
     let negative= wro*(-1);
     let fin = positive + negative;
     return fin
 }
 
-let accuracy= function (att){
+let accuracy= function (att, wro){
     let total= 75;
-    si = (75-att)
+    si = (att-wro)
     accurate = (si/75)
 
     return accurate*100
